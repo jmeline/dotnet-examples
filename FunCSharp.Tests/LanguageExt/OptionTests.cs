19d09911a251e5ff6434a284ad326ef68ae8cb9d
Jacob Meline <6642964+jmeline@users.noreply.github.com>
2024-11-04 08:14:59 -0700

    Playing around with FuncCSharp

--- FunCSharp.Tests/LanguageExt/OptionTests.cs
+++ FunCSharp.Tests/LanguageExt/OptionTests.cs
@@ -1,6 +1,17 @@
+using LanguageExt;
+using Shouldly;
+using Xunit;
+
 namespace FunCSharp.Tests.LanguageExt;
 
 public class OptionTests
 {
-    
+    [Fact]
+    public void SimpleOptions()
+    {
+        Option<int> x = new Some<int>(123);
+        x.ShouldBe(123);
+        x.IsNone.ShouldBeFalse();
+        x.IsSome.ShouldBeTrue();
+    }
 }

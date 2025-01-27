fcd005599f0aa6ba5d987c4681152abc8f8f6c45
Jacob Meline <jmeline@users.noreply.github.com>
2025-01-27 15:51:17 -0700

    Add sample project testing app settings

---
+++ AppSettingsInvestigation/Program.cs
@@ -0,0 +1,66 @@
+using Microsoft.Extensions.Options;
+
+var builder = WebApplication.CreateBuilder(args);
+
+// Add services to the container.
+// Learn more about configuring OpenAPI at https://aka.ms/aspnet/openapi
+builder.Services.AddOpenApi();
+
+var environment = builder.Environment;
+var configuration = builder.Configuration;
+var services = builder.Services;
+
+configuration
+    .SetBasePath(Directory.GetCurrentDirectory())
+    .AddJsonFile("appsettings.json")
+    .AddJsonFile($"appsettings.{environment.EnvironmentName}.json", optional: true)
+    .AddEnvironmentVariables()
+    .Build(); // overrides with Env Variables for K8's
+
+services.AddOptions<AppSettings>()
+    .Bind(configuration.GetSection(nameof(AppSettings)))
+    .ValidateDataAnnotations()
+    .ValidateOnStart();
+
+var app = builder.Build();
+
+var settings = app.Services.GetRequiredService<IOptions<AppSettings>>();
+// Configure the HTTP request pipeline.
+if (app.Environment.IsDevelopment())
+{
+    app.MapOpenApi();
+}
+
+app.UseHttpsRedirection();
+
+var summaries = new[]
+{
+    "Freezing", "Bracing", "Chilly", "Cool", "Mild", "Warm", "Balmy", "Hot", "Sweltering", "Scorching"
+};
+
+app.MapGet("/weatherforecast", () =>
+{
+    var forecast =  Enumerable.Range(1, 5).Select(index =>
+        new WeatherForecast
+        (
+            DateOnly.FromDateTime(DateTime.Now.AddDays(index)),
+            Random.Shared.Next(-20, 55),
+            summaries[Random.Shared.Next(summaries.Length)]
+        ))
+        .ToArray();
+    return forecast;
+})
+.WithName("GetWeatherForecast");
+
+app.Run();
+
+record WeatherForecast(DateOnly Date, int TemperatureC, string? Summary)
+{
+    public int TemperatureF => 32 + (int)(TemperatureC / 0.5556);
+}
+
+public record AppSettings
+{
+    public string AccountNumber { get; set; }
+    public string Name { get; set; }
+}
